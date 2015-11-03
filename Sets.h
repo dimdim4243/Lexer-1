a4e3731fe06fda1d28c14b0fa206d03edf207d9b
Arkadii Berezkin <arkm4n@gmail.com>
2015-11-03 23:42:10 +1000

    Map String->Type and casting keyword and operators

--- Sets.h
+++ Sets.h
@@ -64,7 +64,20 @@ enum TYPES
 #undef optr
 #undef sop
 
+#define kw(k) m[#k] = k;
+#define optr(t) m[#t] = t;
+#define sop(s, t) m[#s] = t;
+map<string, TYPES> mapInit(map<string, TYPES> m)
+{
+	#include "Keywords.h"
+};
+#undef kw
+#undef optr
+#undef sop
 
+#define kw(k) case k: return #k;
+#define optr(t) case t: return #t;
+#define sop(s, t) case t: return s;
 string castType (TYPES type)
 {
 	switch (type)
@@ -78,7 +91,11 @@ string castType (TYPES type)
 		case ident:    return "ident";
 		case sep:       return "sep";
 		case keyword:   return "keyword";
+		#include "Keywords.h"
 	}
 }
+#undef kw
+#undef optr
+#undef sop
 
 #endif
