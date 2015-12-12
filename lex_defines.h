5c596c348c0318dcf2964cbe37e9f895f55e1bc7
Arkadii Berezkin <arkm4n@gmail.com>
2015-12-12 22:36:41 +1000

    Add case keyword

--- lex_defines.h
+++ lex_defines.h
@@ -2,7 +2,7 @@
 #define Keywords \
 kw(BEGIN) kw(FORWARD) kw(DO) kw(ELSE) kw(END) kw(FOR) kw(FUNCTION) kw(IF) kw(ARRAY) kw(OF) kw(PROCEDURE)\
 kw(PROGRAM) kw(RECORD) kw(THEN) kw(TO) kw(TYPE) kw(VAR) kw(WHILE) kw(BREAK) kw(CONTINUE) kw(DOWNTO) kw(EXIT)\
-kw(REPEAT) kw(UNTIL) kw(WITH)
+kw(REPEAT) kw(UNTIL) kw(WITH) kw(CASE)
 #endif //Keywords
 
 #ifndef Separators
